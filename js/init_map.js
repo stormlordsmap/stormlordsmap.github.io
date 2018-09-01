744945c81f9f6c28fdd75b11ae8807762afb8680
pauix <pamich1@gmail.com>
2018-09-01 13:15:27 +0200

    Frist markers up.

--- js/init_map.js
+++ js/init_map.js
@@ -2,7 +2,7 @@
         L.tileLayer('img/map/{z}/{y}-{x}.jpeg', {
             minZoom: 1,
             maxZoom: 4,
-            attribution: 'ITP Map by /u/pauix',
+            attribution: 'SL Map by /u/pauix',
             zomSnap: 0.25,
             tms: false,  
 			noWrap: true,
@@ -13,62 +13,53 @@
 
 		  
 		/*This function gives you Lat Long of what you click on */
-		
+		/*
 		function onMapClick(e) {
-	//		if(confirm("ADD MARKER HERE?")){
+			if(confirm("ADD MARKER HERE?")){
 				var name = prompt("Name")
                 var type = prompt("Type")
-				mark = new L.Marker([e.latlng.lat, e.latlng.lng], {icon: eval("Reach"+type+"_"+4)}).bindPopup("txt");
+				mark = new L.Marker([e.latlng.lat, e.latlng.lng], {icon: eval(type+"_"+4)}).bindPopup("txt");
 				map.addLayer(mark)
-              $("#text").val($("#text").val()+"["+e.latlng.lat+","+e.latlng.lng+",\""+name+"\",\""+type+"_4\",\""+type+"_3\",\""+type+"_2\"],\n")
+              $("#text").val($("#text").val()+"["+e.latlng.lat+","+e.latlng.lng+",\""+name+"\"],\n")
         
          //       circle = L.circle([e.latlng.lat, e.latlng.lng],(1), {color: "#ffcc00", fillColor:"#ffcc00",fillOpacity:0.75}).addTo(map)
 		//		$("#text").val($("#text").val()+"["+e.latlng.lat+","+e.latlng.lng+"],\n")
 	
-	//		}
+			}
 
 		};
 		map.on('click', onMapClick);
+        */
         
-        
-        
-        var keeps = [];
-        $.merge(keeps,reachKeeps);
-       //keeps.concat(reachKeeps);
-        
-        var villages = []
-        $.merge(villages,reachVillages);
-        
-        var resources = []
-        $.merge(resources,reachResources);
-		
-
 		
 		//Loop through the keep markers (declared on marker.js) array and add markers to layer
 		var arrKeeps_4 = []
         var arrKeeps_3 = []
         var arrKeeps_2 = []
         
-		//Loop through the village markers (declared on marker.js) array and add markers to layer
-		var arrVillages_4 = []
+        var arrTowns_4 = []
+        var arrTowns_3 = []
+        var arrTowns_2 = []        
+
+        var arrBurgs_4 = []
+        var arrBurgs_3 = []
+        var arrBurgs_2 = []
+        
+        var arrVillages_4 = []
         var arrVillages_3 = []
-        var arrVillages_2 = []        
-
-		//Loop through the resource markers (declared on marker.js) array and add markers to layer
-		var arrResources_4 = []
-        var arrResources_3 = []
-        var arrResources_2 = []        
-
+        var arrVillages_2 = []
+        
+        // KEEPS
         for (var i=0; i<keeps.length; i++) {
            
             var lat = keeps[i][0];
             var lon = keeps[i][1]; 
             var popupText = keeps[i][2];
             
-             var marker_4 = new L.Marker([lat, lon], {icon: keeps[i][3], title:popupText});//.on('click',function(e){$("#extras").html("<iframe style='width:100%;height:100%' src='https://itp.wikia.com/"+this._icon.title+"'></iframe>")});
-             var marker_3 = new L.Marker([lat, lon], {icon: keeps[i][4], title:popupText});//.on('click',function(e){$("#extras").html("<iframe style='width:100%;height:100%' src='https://itp.wikia.com/"+this._icon.title+"'></iframe>")});
-             var marker_2 = new L.Marker([lat, lon], {icon: keeps[i][5], title:popupText});//.on('click',function(e){$("#extras").html("<iframe style='width:100%;height:100%' src='https://itp.wikia.com/"+this._icon.title+"'></iframe>")});
-             
+            var marker_4 = new L.Marker([lat, lon], {icon: Keep_4, title:popupText});//.on('click',function(e){$("#extras").html("<iframe style='width:100%;height:100%' src='https://itp.wikia.com/"+this._icon.title+"'></iframe>")});
+            var marker_3 = new L.Marker([lat, lon], {icon: Keep_3, title:popupText});//.on('click',function(e){$("#extras").html("<iframe style='width:100%;height:100%' src='https://itp.wikia.com/"+this._icon.title+"'></iframe>")});
+            var marker_2 = new L.Marker([lat, lon], {icon: Keep_2, title:popupText});//.on('click',function(e){$("#extras").html("<iframe style='width:100%;height:100%' src='https://itp.wikia.com/"+this._icon.title+"'></iframe>")});
+           
 			arrKeeps_4.push(marker_4)
             arrKeeps_3.push(marker_3)
             arrKeeps_2.push(marker_2)
@@ -76,104 +67,112 @@
 		var keepLayer_4 = L.layerGroup(arrKeeps_4);
         var keepLayer_3 = L.layerGroup(arrKeeps_3);
         var keepLayer_2 = L.layerGroup(arrKeeps_2);
-
         
-       for (var i=0; i<villages.length; i++) {
+        // TOWNS
+        for (var i=0; i<towns.length; i++) {
+           
+            var lat = towns[i][0];
+            var lon = towns[i][1]; 
+            var popupText = towns[i][2];
+            
+             var marker_4 = new L.Marker([lat, lon], {icon: Town_4, title:popupText});//.on('click',function(e){$("#extras").html("<iframe style='width:100%;height:100%' src='https://itp.wikia.com/"+this._icon.title+"'></iframe>")});
+             var marker_3 = new L.Marker([lat, lon], {icon: Town_3, title:popupText});//.on('click',function(e){$("#extras").html("<iframe style='width:100%;height:100%' src='https://itp.wikia.com/"+this._icon.title+"'></iframe>")});
+             var marker_2 = new L.Marker([lat, lon], {icon: Town_2, title:popupText});//.on('click',function(e){$("#extras").html("<iframe style='width:100%;height:100%' src='https://itp.wikia.com/"+this._icon.title+"'></iframe>")});
+            
+			arrTowns_4.push(marker_4)
+            arrTowns_3.push(marker_3)
+            arrTowns_2.push(arrTowns_2)
+        }
+		var townLayer_4 = L.layerGroup(arrTowns_4);
+        var townLayer_3 = L.layerGroup(arrTowns_3);
+        var townLayer_2 = L.layerGroup(arrKeeps_2);
+
+
+        // BURGS
+        for (var i=0; i<burgs.length; i++) {
+           
+            var lat = burgs[i][0];
+            var lon = burgs[i][1]; 
+            var popupText = burgs[i][2];
+            
+             var marker_4 = new L.Marker([lat, lon], {icon: Burg_4, title:popupText});//.on('click',function(e){$("#extras").html("<iframe style='width:100%;height:100%' src='https://itp.wikia.com/"+this._icon.title+"'></iframe>")});
+             var marker_3 = new L.Marker([lat, lon], {icon: Burg_3, title:popupText});//.on('click',function(e){$("#extras").html("<iframe style='width:100%;height:100%' src='https://itp.wikia.com/"+this._icon.title+"'></iframe>")});
+             var marker_2 = new L.Marker([lat, lon], {icon: Burg_2, title:popupText});//.on('click',function(e){$("#extras").html("<iframe style='width:100%;height:100%' src='https://itp.wikia.com/"+this._icon.title+"'></iframe>")});
+            
+			arrBurgs_4.push(marker_4)
+            arrBurgs_3.push(marker_3)
+            arrBurgs_2.push(marker_2)
+        }
+		var burgLayer_4 = L.layerGroup(arrBurgs_4);
+        var burgLayer_3 = L.layerGroup(arrBurgs_3);
+        var burgLayer_2 = L.layerGroup(arrBurgs_2);     
+
+
+        // VILLAGES
+        for (var i=0; i<villages.length; i++) {
            
             var lat = villages[i][0];
             var lon = villages[i][1]; 
             var popupText = villages[i][2];
             
-             var marker_4 = new L.Marker([lat, lon], {icon: villages[i][3]}).bindPopup(popupText);
-             var marker_3 = new L.Marker([lat, lon], {icon: villages[i][4]}).bindPopup(popupText);
-             var marker_2 = new L.Marker([lat, lon], {icon: villages[i][5]}).bindPopup(popupText);
-             
+             var marker_4 = new L.Marker([lat, lon], {icon: Village_4, title:popupText});//.on('click',function(e){$("#extras").html("<iframe style='width:100%;height:100%' src='https://itp.wikia.com/"+this._icon.title+"'></iframe>")});
+             var marker_3 = new L.Marker([lat, lon], {icon: Village_3, title:popupText});//.on('click',function(e){$("#extras").html("<iframe style='width:100%;height:100%' src='https://itp.wikia.com/"+this._icon.title+"'></iframe>")});
+             var marker_2 = new L.Marker([lat, lon], {icon: Village_2, title:popupText});//.on('click',function(e){$("#extras").html("<iframe style='width:100%;height:100%' src='https://itp.wikia.com/"+this._icon.title+"'></iframe>")});
+            
 			arrVillages_4.push(marker_4)
             arrVillages_3.push(marker_3)
             arrVillages_2.push(marker_2)
         }
 		var villageLayer_4 = L.layerGroup(arrVillages_4);
         var villageLayer_3 = L.layerGroup(arrVillages_3);
-        var villageLayer_2 = L.layerGroup(arrVillages_2);
+        var villageLayer_2 = L.layerGroup(arrVillages_2);    
         
-        
-        
-       for (var i=0; i<resources.length; i++) {
-           
-            var lat = resources[i][0];
-            var lon = resources[i][1]; 
-            var popupText = resources[i][2];
-            
-             var marker_4 = new L.Marker([lat, lon], {icon: resources[i][3]}).bindPopup(popupText);
-             var marker_3 = new L.Marker([lat, lon], {icon: resources[i][4]}).bindPopup(popupText);
-             var marker_2 = new L.Marker([lat, lon], {icon: resources[i][5]}).bindPopup(popupText);
-             
-			arrResources_4.push(marker_4)
-            arrResources_3.push(marker_3)
-            arrResources_2.push(marker_2)
-        }
-		var resourceLayer_4 = L.layerGroup(arrResources_4);
-        var resourceLayer_3 = L.layerGroup(arrResources_3);
-        var resourceLayer_2 = L.layerGroup(arrResources_2);
-        
+
         
         
         map.on('zoomend',function(e,m){
             e.target.removeLayer(keepLayer_2);
             e.target.removeLayer(keepLayer_3);
             e.target.removeLayer(keepLayer_4);
+        
+            e.target.removeLayer(townLayer_2);
+            e.target.removeLayer(townLayer_3);
+            e.target.removeLayer(townLayer_4);
+            
+            e.target.removeLayer(burgLayer_2);
+            e.target.removeLayer(burgLayer_3);
+            e.target.removeLayer(burgLayer_4);
             
             e.target.removeLayer(villageLayer_2);
             e.target.removeLayer(villageLayer_3);
             e.target.removeLayer(villageLayer_4);
-            
-            e.target.removeLayer(resourceLayer_2);
-            e.target.removeLayer(resourceLayer_3);
-            e.target.removeLayer(resourceLayer_4);            
-
+ 
             switch(e.target._zoom){
                 case 1:
                 break;
                 
                 case 2:
-                    if(showKeeps) e.target.addLayer(keepLayer_2);
-                    if(showVillages) e.target.addLayer(villageLayer_2);
-                    if(showResources) e.target.addLayer(resourceLayer_2);
+                    e.target.addLayer(keepLayer_2);
+                    e.target.addLayer(townLayer_2);
+                    e.target.addLayer(burgLayer_2);
+                    e.target.addLayer(villageLayer_2);
                 break;
                 
                 case 3:
-                    if(showKeeps) e.target.addLayer(keepLayer_3);
-                    if(showVillages) e.target.addLayer(villageLayer_3);
-                    if(showResources) e.target.addLayer(resourceLayer_3);
+                    e.target.addLayer(keepLayer_3);
+                    e.target.addLayer(townLayer_3);
+                    e.target.addLayer(burgLayer_3);
+                    e.target.addLayer(villageLayer_3);
                 break;
                 
                 case 4:
-                    if(showKeeps) e.target.addLayer(keepLayer_4);
-                    if(showVillages) e.target.addLayer(villageLayer_4);
-                    if(showResources) e.target.addLayer(resourceLayer_4);
+                    e.target.addLayer(keepLayer_4);
+                    e.target.addLayer(townLayer_4);
+                    e.target.addLayer(burgLayer_4);
+                    e.target.addLayer(villageLayer_4);
               break;
             }
-            currentzoom = e.target._zoom;
-            
+            currentzoom = e.target._zoom;            
         })
         
 var currentzoom = 1;
- function toggle(type){
-      switch(type){
-          case "keep":
-            showKeeps = !showKeeps;
-         break;
-
-         case "village":
-            showVillages = !showVillages;
-         break;
-
-        case "resource":
-            showResources = !showResources;
-         break;
-      }
-      layer = eval(type+"Layer_"+currentzoom);
-
-    if (map.hasLayer(layer) && currentzoom>1)     map.removeLayer(layer);
-    else   map.addLayer(layer);
-}
--- js/markers.js
+++ js/markers.js
@@ -1,49 +1,54 @@
   /********************************************/
  /**				MARKERS					**/
 /********************************************/
-/*
+
 // Define marker icons
-var Keep_4 = L.icon({iconUrl: 'img/marker/keep.png',iconSize: [12, 18]})
-var Keep_3 = L.icon({iconUrl: 'img/marker/keep.png',iconSize: [6, 9]})
-var Keep_2 = L.icon({iconUrl: 'img/marker/keep.png',iconSize: [3, 5]})
+var Keep_4 = new L.icon({iconUrl: 'img/marker/keep.png',iconSize: [36, 36]})
+var Keep_3 = new L.icon({iconUrl: 'img/marker/keep.png',iconSize: [18, 18]})
+var Keep_2 = new L.icon({iconUrl: 'img/marker/keep.png',iconSize: [9, 9]})
 
 
 // Define marker icons
-var Village_4 = L.icon({iconUrl: 'img/marker/village.png',iconSize: [12, 18]})
-var Village_3 = L.icon({iconUrl: 'img/marker/village.png',iconSize: [6, 9]})
-var Village_2 = L.icon({iconUrl: 'img/marker/village.png',iconSize: [3, 5]})
+var Village_4 = new L.icon({iconUrl: 'img/marker/village.png',iconSize: [36, 36]})
+var Village_3 = new L.icon({iconUrl: 'img/marker/village.png',iconSize: [18, 18]})
+var Village_2 = new L.icon({iconUrl: 'img/marker/village.png',iconSize: [9, 9]})
 
 // Define marker icons
-var Resource_4 = L.icon({iconUrl: 'img/marker/resource.png',iconSize: [12, 18]})
-var Resource_3 = L.icon({iconUrl: 'img/marker/resource.png',iconSize: [6, 9]})
-var Resource_2 = L.icon({iconUrl: 'img/marker/resource.png',iconSize: [3, 5]})
+var Burg_4 = new L.icon({iconUrl: 'img/marker/burg.png',iconSize: [36, 36]})
+var Burg_3 = new L.icon({iconUrl: 'img/marker/burg.png',iconSize: [18, 18]})
+var Burg_2 = new L.icon({iconUrl: 'img/marker/burg.png',iconSize: [9, 9]})
+
+// Define marker icons
+var Town_4 = new L.icon({iconUrl: 'img/marker/town.png',iconSize: [36, 36]})
+var Town_3 = new L.icon({iconUrl: 'img/marker/town.png',iconSize: [18, 18]})
+var Town_2 = new L.icon({iconUrl: 'img/marker/town.png',iconSize: [9, 9]})
+
+
+
+
+
 
 
 var keeps = [
-[-83.7825325736073,-115.31249999999999,"Starfall"],
-[-82.29712096163482,-112.32421875,"High Hermitage"],
-[-80.57534566635732,-106.25976562499999,"Blackmont"],
-[-80.26825877186883,-94.306640625,"Skyreach"],
-[-81.10681142336675,-94.130859375,"Kingsgrave"],
-[-83.22606748157784,-97.294921875,"Sandstone"], 
+[-69.09993967425088,-48.076171875,"Blackhaven"],
 ]
 
+var towns = [
+
+]
+
+var burgs = [
+[-67.74275906666388,-37.880859375,"Burg"],
+]
 
 var villages = [
-[-82.35580019800932,-58.447265625,"Village1"],
-[-83.31873282163234,-83.671875,"Hellholt"],
-[-83.21569268984949,-64.3359375,"Vaith"],
-[-82.87521777889144,-47.900390625,"Godsgrace"],
-[-83.97925949886205,-52.3828125,"Salt Shore"],
-[-83.60882179529031,-34.453125,"Lemonwood"],
-[-83.3084993040476,-23.90625,"Sunspear"],
+[-72.55449849665266,-50.009765625,"Village"],
+[-64.84893726357946,-45.966796875,"Village"],
+[-59.977005492196,-15.468749999999998,"Village"],
+[-59.88893689676582,-23.642578125,"Village"],
+[-61.73152565113397,-34.013671875,"Village"],
+[-66.33750501996518,-15.644531250000002,"Village"],
+[-69.06856318696032,-23.73046875,"Village"],
+[-63.31268278043484,-23.5546875,"Village"],
+[-66.37275500247455,-27.59765625,"Village"],
 ]
-
-var resources = [
-[-82.43720532313122,-46.845703125,"Resource 1"],
-[-82.46030522503558,-21.005859375,"Spottswood"],
-[-81.76105839473563,-29.8828125,"Toland"],
-[-81.50529869762794,-53.876953125,"Jordayne"],
-[-81.12038840207569,-73.47656249999999,"Yronwood"],
-[-78.56048828398782,-68.818359375,"Wyl"],
-]*/
